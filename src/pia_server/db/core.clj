82a75d2207e51be416b6f2fa21ed3baf9b17ef5a
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-10-15 07:26:39 -0700

    Implement Postgres version of RunStore

--- src/pia_server/db/core.clj
+++ src/pia_server/db/core.clj
@@ -1,9 +1,10 @@
 (ns pia-server.db.core
   (:require [longterm :refer [set-runstore!]]
-            [longterm.runstore :refer [IRunStore]]
+            [longterm.runstore :refer [IRunStore RunStates] :as rs]
             [next.jdbc :as jdbc]
             [environ.core :refer [env]]
-            [hikari-cp.core :as hk]
+            hikari-cp.core
+            [next.jdbc.types :refer [as-other]]
             [next.jdbc.connection :as connection])
   (:import (com.zaxxer.hikari HikariDataSource)
            (java.time LocalDateTime)))
@@ -18,8 +19,8 @@
                          :maximum-pool-size  10
                          :pool-name          "db-pool"
                          :classname          "org.postgresql.Driver"
-                         ;:dbtype             "postgresql"
-                         ;:adapter            "postgresql"
+                         :dbtype             "postgresql"
+                         :adapter            "postgresql"
                          :username           (env :db-username)
                          :password           (env :db-password)
                          :database-name      (env :db-name)
@@ -33,35 +34,43 @@
 (declare run-from-record)
 
 (defmacro with-connection [[conn jdbc-rs] & body]
-  `(let [cp# (:connection-pool ~jdbc-rs)]
-     (with-open [^HikariDataSource ~conn @cp#]
-       ~@body)))
+  `(let [~conn @(:connection-pool ~jdbc-rs)]
+     ; (with-open [^HikariDataSource ~conn @cp#]
+     ~@body))
+
+(defn is-run-state? [state] (some #(= % state) RunStates))
+
+(defn state-to-pg-enum [state]
+  {:pre [(is-run-state? state)]}
+  (as-other (.getName state)))
 
 (defrecord JDBCRunstore [connection-pool]
   IRunStore
   (rs-get [jrs run-id]
-    (with-connection [conn jrs]
-      (jdbc/execute-one! conn ["SELECT * FROM runs WHERE id = ?;" run-id])))
+    (run-from-record
+      (with-connection [conn jrs]
+        (jdbc/execute-one! conn ["SELECT * FROM runs WHERE id = ?;" run-id]))))
 
   (rs-create! [jrs state]
+    {:pre [(is-run-state? state)]}
     (with-connection [conn jrs]
-      (run-from-record (jdbc/execute-one! conn ["INSERT INTO runs (state, stack) VALUES (?, ?) RETURNING runs.*;"
-                                                (.getName state) "[]"]))))
+      (run-from-record (jdbc/execute-one! conn ["INSERT INTO runs (state) VALUES (?) RETURNING runs.*;"
+                                                (state-to-pg-enum state)]))))
   (rs-update! [jrs run]
     (with-connection [conn jrs]
       (let [updated-at (LocalDateTime/now)]
         (jdbc/execute-one!
           conn
           [(str "UPDATE runs "
-                "SET status = ?, result = ?, response = ?, stack = ?, updated_at = ? "
+                "SET state = ?, result = ?, response = ?, stack = ?, updated_at = ? "
                 "WHERE id = ?; ")
-           (.getName (:state run))
-           (prn-str (:result run))
-           (prn-str (:response run))
-           (prn-str (:stack run))
+           (state-to-pg-enum (:state run))
+           (pr-str (:result run))
+           (pr-str (:response run))
+           (pr-str (:stack run))
            updated-at
            (:id run)])
-        run)))
+        (assoc run :updated_at updated-at))))
 
   (rs-unsuspend! [jrs run-id]
     (with-connection [conn jrs]
@@ -70,70 +79,46 @@
           (jdbc/execute-one!
             conn
             [(str "UPDATE runs "
-                  "SET status = 'running', updated_at = ?  WHERE id = ? AND STATUS = 'suspended'"
+                  "SET state = 'running', updated_at = ?  WHERE id = ? AND state = 'suspended'"
                   "RETURNING runs.*;")
              updated-at run-id]))))))
 
 (set-runstore! (JDBCRunstore. connection-pool))
 
 (defn run-from-record [rec]
-  (assoc (longterm.runstore/->Run
-           (:id rec)
-           (read (:stack rec))
-           (keyword (:state rec))
-           (read (:result rec))
-           (read (:response rec)))
-    :created_at (:created_at rec)
-    :updated_at (:updated_at rec)))
-
-;(extend JDBCRunstore
-;
-;  {:rs-get        (fn [jrs run-id]
-;                    (with-connection [conn jrs]
-;                      (jdbc/execute-one! conn ["SELECT * FROM runs WHERE id = ?;" run-id])))
-;
-;   :rs-create!    (fn [jrs state]
-;                    (with-connection [conn jrs]
-;                      (run-from-record (jdbc/execute-one! conn ["INSERT INTO runs (state, stack) VALUES (?, ?) RETURNING runs.*;"
-;                                                                (.getName state) "[]"]))))
-;   :rs-update!    (fn [jrs run]
-;                    (with-connection [conn jrs]
-;                      (let [updated-at (LocalDateTime/now)]
-;                        (jdbc/execute-one!
-;                          conn
-;                          [(str "UPDATE runs "
-;                                "SET status = ?, result = ?, response = ?, stack = ?, updated_at = ? "
-;                                "WHERE id = ?; ")
-;                           (.getName (:state run))
-;                           (prn-str (:result run))
-;                           (prn-str (:response run))
-;                           (prn-str (:stack run))
-;                           updated-at
-;                           (:id run)])
-;                        run)))
-;
-;   :rs-unsuspend! (fn [jrs run-id]
-;                    (with-connection [conn jrs]
-;                      (let [updated-at (LocalDateTime/now)]
-;                        (run-from-record
-;                          (jdbc/execute-one!
-;                            conn
-;                            [(str "UPDATE runs "
-;                                  "SET status = 'running', updated_at = ?  WHERE id = ? AND STATUS = 'suspended'"
-;                                  "RETURNING runs.*;")
-;                             updated-at run-id])))))})
+  (if rec
+    (let [run (longterm.runstore/->Run
+                (:runs/id rec)
+                (if-let [it (:runs/stack rec)] (read-string it))
+                (keyword (:runs/state rec))
+                (if-let [it (:runs/result rec)] (read-string it))
+                (if-let [it (:runs/response rec)] (read-string it)))]
+      (assoc run
+        :created_at (:runs/created_at rec)
+        :updated_at (:runs/updated_at rec)))))
 
 (defn create-db! []
-  (with-open [^HikariDataSource connection @connection-pool]
-    (jdbc/execute! connection ["
+  (jdbc/execute! @connection-pool ["
+      CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";
+      CREATE TYPE IF NOT EXISTS RUNSTATES AS ENUM ('running', 'suspended', 'complete', 'error');
       CREATE TABLE IF NOT EXISTS runs (
-      id UUID PRIMARY KEY,
-      state ENUM('running', 'suspended', 'complete', 'error') DEFAULT 'suspended',
-      stack TEXT DEFAULT '[]',
+      id uuid DEFAULT uuid_generate_v4(),
+      state RUNSTATES,
+      stack TEXT DEFAULT '()',
       result TEXT,
       response TEXT,
       error TEXT,
       error_info TEXT,
       created_at TIMESTAMP  NOT NULL  DEFAULT current_timestamp,
-      updated_at TIMESTAMP  NOT NULL  DEFAULT current_timestamp
-      );"])))
+      updated_at TIMESTAMP  NOT NULL  DEFAULT current_timestamp,
+      PRIMARY KEY (id)
+      );"]))
+
+;; HELPERS for debugging
+(def rs (JDBCRunstore. connection-pool))
+(defn exec! [& args] (jdbc/execute! @connection-pool (vec args)))
+
+(defn uuid [] (java.util.UUID/randomUUID))
+(defn delete-db []
+  (exec! "drop table if exists runs; drop type if exists Runstates;"))
+
