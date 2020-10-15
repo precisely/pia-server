e32003550ecb89576758b5e0d14f93ee1bb5aac4
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-10-15 03:43:52 -0700

    WIP
    * can't get the connection pool to work. I keep getting 'No suitable drive'

--- project.clj
+++ project.clj
@@ -1,11 +1,17 @@
-(require '[clojure.edn :as edn] '[clojure.java.io :as io])
+(require '[clojure.edn :as edn] '[clojure.java.io :as io] '[clojure.string :as str])
 (defn load-edn
   "Load edn from an io/reader source (filename or io/resource)."
   [source]
   (with-open [r (io/reader source)]
     (edn/read (java.io.PushbackReader. r))))
-
 (def envfile (load-edn (io/file "env.edn")))
+(defn env
+  ([key] (env key nil))
+  ([key default]
+  (let [env-var (str/replace (str/upper-case (.getName key)) "-" "_")]
+    (or (System/getenv env-var)
+      (key envfile)
+      default))))
 
 (defproject pia-server "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
@@ -13,22 +19,23 @@
                  [metosin/compojure-api "2.0.0-alpha30"]
                  [seancorfield/next.jdbc "1.1.588"]
                  [org.clojure/tools.logging "1.1.0"]
+                 [org.postgresql/postgresql "42.2.10"]
+                 [hikari-cp "2.13.0"]
                  [environ "1.2.0"]
-                 [com.github.precisely/longterm "0.1.1-SNAPSHOT"]
-
-                 [korma "0.4.3"]
-                 ;[org.postgresql/postgresql "9.2-1002-jdbc4"]
-                 [org.xerial/sqlite-jdbc "3.7.15-M1"]
-                 ]
-  :plugins  [[lein-environ "1.2.0"]]
-  :repositories [["jitpack" {:url "https://jitpack.io"
-                             :username ~(:jetpack-auth-token envfile)
-                             :password "." }]]
+                 [com.github.precisely/longterm "0.1.2"]]
+  :plugins [[lein-environ "1.2.0"]]
+  :repositories [["jitpack" {:url      "https://jitpack.io"
+                             :username ~(env :jetpack-auth-token)
+                             :password "."}]]
   :main pia-server.handler
   :ring {:handler pia-server.handler/app}
   :uberjar-name "server.jar"
-  :uberwar-name "server.war"
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "3.1.0"]
                                   [ring/ring-mock "0.3.2"]]
-                   :plugins      [[lein-ring "0.12.5"]]}}
+                   :plugins      [[lein-ring "0.12.5"]]
+                   :env          {:db-username     ~(env :db-username (System/getProperty "user.name"))
+                                  :password        ~(env :db-password "")
+                                  :database-name   ~(env :db-name "pia_runstore")
+                                  :server-name     ~(env :db-server-name "localhost")
+                                  :port-number     ~(env :db-port "5432")}}}
   :jvm-opts ["-Dclojure.tools.logging.factory=clojure.tools.logging.impl/slf4j-factory"])
--- src/pia_server/db/core.clj
+++ src/pia_server/db/core.clj
@@ -1,52 +1,139 @@
 (ns pia-server.db.core
-  (:require [korma.core :refer :all]
-            [korma.db :refer [defdb]]
-            [longterm :refer [set-runstore!]])
-  (:import (longterm.runstore IRunStore IRun)))
+  (:require [longterm :refer [set-runstore!]]
+            [longterm.runstore :refer [IRunStore]]
+            [next.jdbc :as jdbc]
+            [environ.core :refer [env]]
+            [hikari-cp.core :as hk]
+            [next.jdbc.connection :as connection])
+  (:import (com.zaxxer.hikari HikariDataSource)
+           (java.time LocalDateTime)))
 
-(defrecord KormaRunStore [korma-db])
-(extend KormaRunStore
+(def datasource-options {:auto-commit        true
+                         :read-only          false
+                         :connection-timeout 30000
+                         :validation-timeout 5000
+                         :idle-timeout       600000
+                         :max-lifetime       1800000
+                         :minimum-idle       10
+                         :maximum-pool-size  10
+                         :pool-name          "db-pool"
+                         :classname          "org.postgresql.Driver"
+                         ;:dbtype             "postgresql"
+                         ;:adapter            "postgresql"
+                         :username           (env :db-username)
+                         :password           (env :db-password)
+                         :database-name      (env :db-name)
+                         :server-name        (env :db-server-name)
+                         :port-number        (Integer/parseInt (env :db-port "5432"))
+                         :register-mbeans    false})
+
+(def connection-pool (delay (connection/->pool HikariDataSource
+                                               datasource-options)))
+
+(declare run-from-record)
+
+(defmacro with-connection [[conn jdbc-rs] & body]
+  `(let [cp# (:connection-pool ~jdbc-rs)]
+     (with-open [^HikariDataSource ~conn @cp#]
+       ~@body)))
+
+(defrecord JDBCRunstore [connection-pool]
   IRunStore
-  {:rs-get (fn [krs run-id] (select))
-   :rs-create! (fn [krs state])
-   :rs-update! (fn [krs run])
-   :rs-unsuspend! (fn [krs run-id])})
+  (rs-get [jrs run-id]
+    (with-connection [conn jrs]
+      (jdbc/execute-one! conn ["SELECT * FROM runs WHERE id = ?;" run-id])))
 
-(defrecord KormaRun [run-entity])
-(extend KormaRun
-  IRun
-  {:id (fn [kr] (:id kr))
-   :state (fn [kr] (:state kr))
-   :result (fn [kr] (:result kr))})
-(defentity Run
-  (pk :id)
-  ;; it's used for relationships joins.
-  (table :users) ;; by default the name of the symbol.
-  ;; The line above is also unecessary.
-  (database db) ;; if none is specified the last defdb
-  ;; will be used. Also unnecessary.
-  (entity-fields :id :state :response :result :stack :error :err-info :created_on :updated_on) ; default select
+  (rs-create! [jrs state]
+    (with-connection [conn jrs]
+      (run-from-record (jdbc/execute-one! conn ["INSERT INTO runs (state, stack) VALUES (?, ?) RETURNING runs.*;"
+                                                (.getName state) "[]"]))))
+  (rs-update! [jrs run]
+    (with-connection [conn jrs]
+      (let [updated-at (LocalDateTime/now)]
+        (jdbc/execute-one!
+          conn
+          [(str "UPDATE runs "
+                "SET status = ?, result = ?, response = ?, stack = ?, updated_at = ? "
+                "WHERE id = ?; ")
+           (.getName (:state run))
+           (prn-str (:result run))
+           (prn-str (:response run))
+           (prn-str (:stack run))
+           updated-at
+           (:id run)])
+        run)))
 
+  (rs-unsuspend! [jrs run-id]
+    (with-connection [conn jrs]
+      (let [updated-at (LocalDateTime/now)]
+        (run-from-record
+          (jdbc/execute-one!
+            conn
+            [(str "UPDATE runs "
+                  "SET status = 'running', updated_at = ?  WHERE id = ? AND STATUS = 'suspended'"
+                  "RETURNING runs.*;")
+             updated-at run-id]))))))
 
+(set-runstore! (JDBCRunstore. connection-pool))
 
-  ())
+(defn run-from-record [rec]
+  (assoc (longterm.runstore/->Run
+           (:id rec)
+           (read (:stack rec))
+           (keyword (:state rec))
+           (read (:result rec))
+           (read (:response rec)))
+    :created_at (:created_at rec)
+    :updated_at (:updated_at rec)))
 
-(extend
-  DBConnection
+;(extend JDBCRunstore
+;
+;  {:rs-get        (fn [jrs run-id]
+;                    (with-connection [conn jrs]
+;                      (jdbc/execute-one! conn ["SELECT * FROM runs WHERE id = ?;" run-id])))
+;
+;   :rs-create!    (fn [jrs state]
+;                    (with-connection [conn jrs]
+;                      (run-from-record (jdbc/execute-one! conn ["INSERT INTO runs (state, stack) VALUES (?, ?) RETURNING runs.*;"
+;                                                                (.getName state) "[]"]))))
+;   :rs-update!    (fn [jrs run]
+;                    (with-connection [conn jrs]
+;                      (let [updated-at (LocalDateTime/now)]
+;                        (jdbc/execute-one!
+;                          conn
+;                          [(str "UPDATE runs "
+;                                "SET status = ?, result = ?, response = ?, stack = ?, updated_at = ? "
+;                                "WHERE id = ?; ")
+;                           (.getName (:state run))
+;                           (prn-str (:result run))
+;                           (prn-str (:response run))
+;                           (prn-str (:stack run))
+;                           updated-at
+;                           (:id run)])
+;                        run)))
+;
+;   :rs-unsuspend! (fn [jrs run-id]
+;                    (with-connection [conn jrs]
+;                      (let [updated-at (LocalDateTime/now)]
+;                        (run-from-record
+;                          (jdbc/execute-one!
+;                            conn
+;                            [(str "UPDATE runs "
+;                                  "SET status = 'running', updated_at = ?  WHERE id = ? AND STATUS = 'suspended'"
+;                                  "RETURNING runs.*;")
+;                             updated-at run-id])))))})
 
-(defn create-tables
-  "Create a factoid table"
-  [db]
-  (sql/db-do-commands
-    db
-    (sql/create-table-ddl
-      :run
-      [[:id          "UUID" "IDENTITY" "NOT NULL" "PRIMARY KEY" "uuid_generate_v4()"]
-       [:state       "ENUM" "('running','suspended','complete','error')"]
-       [:response    "TEXT"]
-       [:result      "TEXT"]
-       [:stack       "TEXT"]
-       [:error       "TEXT"] ; error message reportable to API consumer
-       [:err-info    "TEXT"] ; detailed debugging info
-       [:created_on  "TIMESTAMP" "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]
-       [:updated_on  "TIMESTAMP" "NOT NULL" "DEFAULT CURRENT_TIMESTAMP"]])))
+(defn create-db! []
+  (with-open [^HikariDataSource connection @connection-pool]
+    (jdbc/execute! connection ["
+      CREATE TABLE IF NOT EXISTS runs (
+      id UUID PRIMARY KEY,
+      state ENUM('running', 'suspended', 'complete', 'error') DEFAULT 'suspended',
+      stack TEXT DEFAULT '[]',
+      result TEXT,
+      response TEXT,
+      error TEXT,
+      error_info TEXT,
+      created_at TIMESTAMP  NOT NULL  DEFAULT current_timestamp,
+      updated_at TIMESTAMP  NOT NULL  DEFAULT current_timestamp
+      );"])))
