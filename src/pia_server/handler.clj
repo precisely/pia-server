a9917f8264101b74c158263f6c5aea6a3f2b1c13
Aneil Mallavarapu <aneilmallavar@gmail.com>
2020-10-15 19:54:22 -0700

    Fix API errors because of extra keys

--- src/pia_server/handler.clj
+++ src/pia_server/handler.clj
@@ -3,7 +3,9 @@
             [ring.util.http-response :refer :all]
             [longterm :refer :all]
             [pia-server.db :as db]
-            [schema.core :as s]))
+            [schema.core :as s]
+            pia-server.select-coercion
+            [compojure.api.coercion.core :as cc]))
 
 (s/defschema Run
   {:id       s/Uuid
@@ -11,27 +13,32 @@
    :result   s/Any
    :response s/Any})
 
-(s/defschema StartForm
-  {:flow s/Symbol
-   :args [s/Any]})
-
 (s/defschema Event
-  {:event-id s/Keyword
-   :data     s/Any})
+  {:event-id              s/Keyword
+   (s/optional-key :data) s/Any})
 
 (def db-runstore (db/make-runstore))
 (set-runstore! db-runstore)
 
-(deflow foo [] (respond! "hello...") (suspend! :user) (respond! "world!"))
+(deflow foo []
+  (respond! "hello...")
+  (suspend! :foo)
+  (respond! "world!"))
+
 (def flows {:foo foo})
+
+(defn run-result [run]
+  (select-keys run [:id :state :result :response]))
+
 (def app
   (api
     {:swagger
-     {:ui   "/"
-      :spec "/swagger.json"
-      :data {:info {:title       "pia-server"
-                    :description "Precisely Intelligent Agent Server API"}
-             :tags [{:name "api", :description "some apis"}]}}}
+               {:ui   "/"
+                :spec "/swagger.json"
+                :data {:info {:title       "pia-server"
+                              :description "Precisely Intelligent Agent Server API"}
+                       :tags [{:name "api", :description "some apis"}]}}
+     :coercion :schema}
 
     (context "/api" []
       :tags ["api"]
@@ -43,21 +50,21 @@
           :path-params [id :- s/Uuid]
           :return Run
           :summary "gets a run"
-          (ok (get-run id)))
+          (ok (run-result (get-run id))))
 
         (POST "/:flow" []
           :path-params [flow :- s/Keyword]
           :return Run
           :body [args [s/Any]]
           :summary "starts a Run based on the given flow"
-          (ok (apply start-flow! (get flows flow) args)))
+          (ok (run-result (apply start-flow! (get flows flow) args))))
 
         (POST "/:id/continue" []
           :path-params [id :- s/Uuid]
           :return Run
           :body [event Event]
           :summary "continues a run"
-          (ok (apply process-event!
-                     id
-                     (:event-id event)
-                     (:data event))))))))
+          (ok (run-result (apply process-event!
+                                 id
+                                 (:event-id event)
+                                 (:data event)))))))))
