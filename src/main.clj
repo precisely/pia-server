0b1d9319c81b54c49294c6e565624c716e3fe4cb
Constantine Vetoshev <constantine@precise.ly>
2020-10-30 19:49:35 -0700

    Add entry point file.

---
+++ src/main.clj
@@ -0,0 +1,29 @@
+(ns pia-server.main
+  (:require [ring.adapter.jetty :as jetty]
+            [envvar.core :as envvar :refer [env]])
+  (:require [pia-server.handler :as handler])
+  ;; FIXME: Require app from handler.clj, maybe rename to app.clj?
+  (:gen-class))
+
+
+;;; TODO: Consider using the Component framework. Things which need to be
+;;; managed:
+;;;
+;;; - application server
+;;; - database connection pool
+
+
+(defonce ^{:dynamic true} *server* (atom nil))
+
+(defn start [app & {:keys [port join?]
+                    :or {port 8080, join? false}}]
+  (reset! *server* (jetty/run-jetty app {:port port, :join? join?})))
+
+(defn stop []
+  (when-not (nil? @*server*)
+    (.stop @*server*)
+    (reset! *server* nil)))
+
+(defn -main [& args]
+  (let [port (get @env :port 8080)]
+    (start handler/app :port port, :join? true)))
