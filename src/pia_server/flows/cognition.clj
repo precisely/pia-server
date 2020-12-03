598bf24b4cbd880e8e4ca41b5a7914aa61f3cf94
oiiio <gareth862@gmail.com>
2020-12-03 14:10:27 -0600

    fixing some typos

--- src/pia_server/flows/cognition.clj
+++ src/pia_server/flows/cognition.clj
@@ -3,9 +3,10 @@
             [pia-server.flows.components :refer :all]
             ))
 
-;; Assesses the patient's post-exertional malaise
+;; Assesses the patient's cognitive impairment
 ;; Dropout bucket is fatigue-iof-6m-pem-sleep HOWEVER all patients should still be provided the orthostatic test and given referrals due
 ;; to their "likely CFS" state
+;; Also, patients that show significant cognitive impairment are bucketed as fatigue-iof-6m-pem-sleep-ci
 ;; There are two UX strategies we could use here
 ;; 1) The questions in these flows end if any receive freq and severity of 2 or more (this is what is present here)
 ;; 2) Ask all question and compute score thresholds after, which provides more symptom tracking data that could be useful
@@ -21,20 +22,27 @@
 
 
 (def cognition-questions {
-                    '"Problems remembering things" "How severe is this feeling?",
-                    '"Need to nap daily" "Can you provide how severe this urge is?",
-                    '"Problems falling acognition" "Please rate how severe these problems are",
-                    '"Problems staying asleep" "And now how severe is this",
-                    '"Waking up early in the morning (e.g. 3 am)" "Can you rate how severe this is?",
-                    '"Sleep during day and stay awake during night" "What is the severity of this problem?"
-                    }
+                          '"Problems remembering things"                                          "How severe is this?",
+                          '"Difficulty paying attention for a long period of time"                "Can you rate the severity of this difficulty?",
+                          '"Difficulty finding the right word to say or expressing thoughts"      "Please rate how severe these problems are",
+                          '"Difficulty understanding things"                                      "And now how severe is this?",
+                          '"Only able to focus on one thing at a time"                            "Can you rate this inability?",
+                          '"Unable to focus vision"                                               "How severe is this inability?",
+                          '"Unable to focus attention"                                            "Please give the severity of this",
+                          '"Loss of depth perception"                                             "How bad is this loss?",
+                          '"Slowness of thought"                                                  "How would you rate the severity of this slowness?",
+                          '"Absent-mindedness or forgetfulness"                                   "Can you give the severity of this problem?",
+                          '"Feeling disoriented"                                                  "How severe is this feeling?",
+                          '"Slowed speech"                                                        "Please rate how bad this slowness is",
+                          '"Difficulty reading (dyslexia) after mild physical or mental activity" "How you describe the severity of this difficulty?"
+                          }
   )
 
 
 
 (deflow no-cognition []
         (*> "Thanks for taking the time to answer these questions until this point",
-            "Your sleep issues appear to be within normal ranges right now, but you should still keep track of your symptoms",
+            "Your cognitive issues appear to be within normal ranges right now, but you should still keep track of your symptoms",
             "To help you with that, you can come back here to answer some more questions when you see a notification",
             "Let's make sure you recover your energy!"))
 
@@ -52,10 +60,12 @@
                 sev (<*)
                 ]
             (if (and (>= freq 2) (>= sev 2))
-              (*> "Based on these responses, your sleep problems indicate a possibly serious issue",
-                  "You would benefit greatly from completing the next questions, and overall you are almost finished with the assessment")
+              (*> "Based on these responses, your cognitive fatigue also indicates the need for a referral",
+                  "Now let's move on to the last (short) part. Almost done!"
+                  )
               (if (rest questions)
                 (recur (first questions) (rest questions))
-                (no-cognition)
+                (*> "Your cognitive impairment is in normal ranges, however you are still at a high risk of developing chronic fatigue",
+                    "Now let's move on to the last (short) part. Almost done!")
                 )))
               ))
