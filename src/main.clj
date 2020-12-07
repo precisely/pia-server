382d333e9eff8b867c2ebf9dcf9fd6583a45cecb
oiiio <gareth862@gmail.com>
2020-12-07 12:21:01 -0600

    swapping to master version

--- src/main.clj
+++ src/main.clj
@@ -1,11 +1,11 @@
 (ns main
-  (:require [ring.adapter.jetty :as jetty]
-            [envvar.core :refer [env]]
-            [pia-server.core :as pia]
-            [taoensso.timbre :as log]
-            [pia-server.db :as db]
-            [pia-server.expiry-monitor :as expiry-monitor])
-  (:gen-class))
+    (:require [ring.adapter.jetty :as jetty]
+      [envvar.core :refer [env]]
+      [pia-server.core :as pia]
+      [taoensso.timbre :as log]
+      [pia-server.db :as db]
+      [pia-server.expiry-monitor :as expiry-monitor])
+    (:gen-class))
 
 ;;; TODO: Consider using the Component framework. Things which need to be
 ;;; managed:
@@ -17,17 +17,17 @@
 
 (defn start [app & {:keys [port join?]
                     :or   {port 8080, join? false}}]
-  (log/info "Starting pia-server")
-  (db/start-connection-pool!)
-  (db/create-db!)
-  ;(expiry-monitor/start 1000)
-  (reset! *server* (jetty/run-jetty app {:port port, :join? join?})))
+      (log/info "Starting pia-server")
+      (db/start-connection-pool!)
+      (db/create-db!)
+      ;(expiry-monitor/start 1000)
+      (reset! *server* (jetty/run-jetty app {:port port, :join? join?})))
 
 (defn stop []
-  (when-not (nil? @*server*)
-    (.stop @*server*)
-    (reset! *server* nil)))
+      (when-not (nil? @*server*)
+                (.stop @*server*)
+                (reset! *server* nil)))
 
 (defn -main [& args]
-  (let [port (read-string (get @env :port 8080))]
-    (start #'pia/app :port port, :join? true)))
+      (let [port (read-string (get @env :port 8080))]
+           (start #'pia/app :port port, :join? true)))
