9b4298b1e2b3857f9696d873539f4151ff1f4336
Constantine Vetoshev <constantine@precise.ly>
2020-12-28 04:59:01 -0800

    Add basic validate and create.

--- src/pia_server/hl7-playground.clj
+++ src/pia_server/hl7-playground.clj
@@ -1,10 +1,11 @@
 (ns pia-server.hl7-playground
   (:require [clojure.data.json :as json]
-            [json-schema.core :as json-schema]))
+            [json-schema.core :as json-schema]
+            [pia-server.db-hl7 :as db-hl7]))
 
 
 (def r4-schema-json (clojure.java.io/resource "hl7-schemas/r4.json"))
-(def r4-schema (json/read (clojure.java.io/reader r4-schema)))
+(def r4-schema (json/read (clojure.java.io/reader r4-schema-json)))
 
 
 (defn run []
@@ -26,3 +27,42 @@
      (try
        (json-schema/validate r4-schema invalid-pt)
        (catch clojure.lang.ExceptionInfo ex (-> (ex-data ex) :errors first)))]))
+
+(defn validate [res]
+  (json-schema/validate r4-schema res))
+
+(defn valid? [res]
+  (try
+    (validate res)
+    true
+    (catch clojure.lang.ExceptionInfo ex false)))
+
+(defn create [res & {:keys [validate?] :or {validate? true}}]
+  (let [res-to-save (if validate? (validate res) res)]
+    (db-hl7/execute-one!
+     "INSERT INTO resources (data) VALUES (to_jsonb(?))"
+     (json/write-str res-to-save))
+    :ok))
+
+;; FIXME: Implement this.
+(defn save [])
+
+;; TODO: Need to support finders, not just ID.
+(defn update-slow [id path val]
+  ;; FIXME: broken
+  (let [res (-> (db-hl7/execute-one!
+                 "SELECT data FROM resources WHERE id = uuid(?)" id)
+                :resources/data
+                str
+                json/read-str
+                json/read-str)
+        updated (assoc-in res path val)]
+    (db-hl7/execute-one!
+     "UPDATE resources SET data = ? WHERE id = uuid(?)"
+     [(json/write-str updated)
+      id])))
+
+(defn update-fast [id path val]
+  ;; FIXME: write this
+  ;; does not validate the schema and uses jsonb_set
+  )
