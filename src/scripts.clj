ffd7bed90385447370470d48341c9f126439f3a3
Constantine Vetoshev <constantine@precise.ly>
2020-12-28 12:07:48 -0800

    Try to fix CI with new database info.

--- src/scripts.clj
+++ src/scripts.clj
@@ -1,7 +1,10 @@
 (ns scripts
   (:require main
-            [pia-server.db-runs :as db-runs]))
+            [pia-server.db-runs :as db-runs]
+            [pia-server.db-hl7 :as db-hl7]))
 
 (defn create-db []
   (db-runs/start-connection-pool!)
-  (db-runs/migrate!))
+  (db-runs/migrate!)
+  (db-hl7/start-connection-pool!)
+  (db-hl7/migrate!))
