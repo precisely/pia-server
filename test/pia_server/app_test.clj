085d38c790b10532f552a3bb26a4f2475f7123f8
Aneil Mallavarapu <aneilmallavar@gmail.com>
2021-01-13 03:55:23 -0800

    Add symbol resolution of rapids Symbols for latest Cursive

---
+++ test/pia_server/app_test.clj
@@ -0,0 +1,46 @@
+(ns pia-server.app-test
+  (:require [pia-server.app :refer :all]
+            [clojure.test :refer :all]
+            [cheshire.core :refer [generate-string parse-string]]
+            [ring.mock.request :as mock]
+            [rapids :refer :all]))
+
+(defn json-request [method resource & {:keys [params body] :or {params []}}]
+  (let [response (app
+                   (-> (mock/request method resource)
+                       (mock/content-type "application/json")
+                       (mock/body (generate-string body))))]
+    (update response :body #(-> % slurp (parse-string keyword)))))
+
+(deflow simple-flow []
+  (*> "hi")
+  (let [result (<*)]
+    (*> (str "I received " result))))
+
+(deflow error-flow []
+  (ex-info "Ooops!" {:type :oops}))
+
+(deftest AppExceptionTest
+  (binding [flows {:simple-flow #'simple-flow
+                   :error-flow #'error-flow}]
+    (testing "input error while starting a flow"
+      (let [{status :status, body :body} (json-request :post "/api/runs/simple-flow" :body "not-an-array")]
+        (is (= status 400))
+        (is (= (:type body) "input"))))
+
+    (testing "starting a flow"
+      (let [{status :status, {id :id, state :state, next_id :next_id, :as body} :body}
+            (json-request :post "/api/runs/simple-flow" :body [])]
+        (is (= status 200))
+        (is (string? id))
+        (is (= id next_id))
+        (is (= state "suspended"))
+
+        (testing "continuing a flow"
+          (let [{status :status, {id :id, state :state, next_id :next-id, :as body} :body}
+                (json-request :post (str "/api/runs/" next_id "/continue")
+                              :body {:data "FOO"})]
+            (is (= status 200))
+            (is (= state "complete"))
+            (is (nil? next_id))
+            (is (= (:response body) ["I received FOO"]))))))))
