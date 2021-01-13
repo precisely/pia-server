0c9369c392622f979ff6f5dd43f2cba3512284f7
Aneil Mallavarapu <aneilmallavar@gmail.com>
2021-01-13 10:59:39 -0800

    Fixing db test

--- test/pia_server/db_runs_test.clj
+++ test/pia_server/db_runs_test.clj
@@ -4,17 +4,19 @@
             [clojure.test :refer :all]
             [clojure.core :as clj]
             [rapids.run :as r]
+            [envvar.core :refer [env keywordize]]
             [next.jdbc :as jdbc]
             [next.jdbc.connection :as connection]
             hikari-cp.core
             [honeysql.core :as sql]
             [honeysql.helpers :refer :all]
             [rapids.runstore :as rs]
-            ;; FIXME: :refer :all above and db here
-            [pia-server.db-runs :as db])
+    ;; FIXME: :refer :all above and db here
+            [pia-server.db-runs :as db]
+            [pia-server.util :as util])
   (:import (com.zaxxer.hikari HikariDataSource)))
 
-                                        ;(def log-level-map
+;(def log-level-map
 ;  {:off     Level/OFF,
 ;   :severe  Level/SEVERE,
 ;   :warning Level/WARNING,
@@ -42,29 +44,39 @@
 ;         #_(doseq [[logger# level#] (interleave loggers# pre-levels#)]
 ;             (if level# (set-logger-level logger# level#)))))))
 
-;; FIXME: This is all wrong. Test database configuration should be dedicated.
 (defonce test-connection-pool
-  (let [options (reduce #(clj/update %1 (first %2) (second %2))
-                  datasource-options
-                  [[:dbname #(str "test_" %)]
-                   [:maximum-pool-size (constantly 2)]
-                   [:pool-name #(str "test_" %)]])]
-    (connection/->pool HikariDataSource options)))
+         (let [options (assoc datasource-options
+                         :jdbcUrl (util/heroku-db-url->jdbc-url
+                                    (get @env
+                                         ;; resolve Heroku indirection
+                                         (-> @env
+                                             (get :db-env-var-runstore)
+                                             keywordize)
+                                         ;; default if no vars are set
+                                         (str "postgres://"
+                                              (System/getProperty "user.name")
+                                              "@localhost:5432/test_pia_runstore"))))
+               #_(reduce #(clj/update %1 (first %2) (second %2))
+                         datasource-options
+                         [[:dbname #(str "test_" %)]
+                          [:maximum-pool-size (constantly 2)]
+                          [:pool-name #(str "test_" %)]])]
+           (connection/->pool HikariDataSource options)))
 
 (defmacro with-test-db [& body]
   `(binding [*connection-pool* test-connection-pool]
      (let [connection# (jdbc/get-connection test-connection-pool)]
        (rapids/with-runstore [(make-runstore connection#)]
-         (db/migrate!)
-         ~@body
-         (db/delete-db!)))))
+                             (db/migrate!)
+                             ~@body
+                             (db/delete-db!)))))
 
 (deftest ^:db write-run
   (testing "can create a run in the database"
     (with-test-db
       (let [[run, rec] (r/make-test-run :id :error)
             db-rec (with-transaction [jrs]
-                     (rs/rs-create! jrs rec))
+                                     (rs/rs-create! jrs rec))
             db-run (r/run-from-record db-rec)]
         (is (= (dissoc run :id) (dissoc db-run :id)))
 
