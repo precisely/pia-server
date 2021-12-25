023e541e64bb435f07461e1ef70c370b8a1484a3
Aneil Mallavarapu <aneilmallavar@gmail.com>
2021-12-25 15:58:53 -0800

    Bump to using rapids 0.6.8

--- project.clj
+++ project.clj
@@ -40,7 +40,7 @@
                  [envvar "1.1.2"]
 
                  ;; application support
-                 [precisely/rapids "0.6.7"]
+                 [precisely/rapids "0.6.8"]
                  [clojure.java-time "0.3.2"]
                  [org.clojure/core.async "1.3.610"]
                  [camel-snake-kebab "0.4.2"]
