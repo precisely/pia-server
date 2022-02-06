2a66e0d1632294c6f5c043c7e0e96909e53e0ac1
Aneil Mallavarapu <aneilmallavar@gmail.com>
2022-02-05 21:44:07 -0800

    Improve anticoag workflow
    * disallow duplicate runs for a patient
    * add some phase statuses

--- src/pia_server/apps/anticoagulation/flows/main.clj
+++ src/pia_server/apps/anticoagulation/flows/main.clj
@@ -24,8 +24,8 @@
          creatinine-level :creatinine-level,
          cbc-platelets    :cbc-platelets} results
         disease-conditions (-> patient :diseases)           ; hypertension, congestive-heart-failure
-        age (-> patient :age)
-        medications (-> patient :medications)]
+        age                (-> patient :age)
+        medications        (-> patient :medications)]
     ;;
     ;; some calculation here...
     ;;
@@ -61,19 +61,21 @@
   }"
   [patient tests]
   {:pre [(p/patient? patient)]}
-  (let [lab (block! (start! common-patient/pick-lab
-                            [patient tests]
-                            :status {:patient-id (:id patient)
-                                     :title      "Choose lab for anticoagulation bloodwork"}))
-        _ (println ">>>>>>>>>>\nLab selected: " lab)
-        labwork-run (start! lab-monitor [lab patient tests] :status {:title      "Anticoagulation lab monitor"
-                                                                     :patient-id (:id patient)})
+  ;; TODO: start the lab monitor from within the patient run
+  (let [lab              (block! (start! common-patient/pick-lab
+                                         [patient tests]
+                                         :status {:patient-id (:id patient)
+                                                  :title      "Choose lab for anticoagulation bloodwork"}))
+        _                (println ">>>>>>>>>>\nLab selected: " lab)
+        labwork-run      (start! lab-monitor [lab patient tests] :status {:title      "Anticoagulation lab monitor"
+                                                                          :patient-id (:id patient)})
         patient-reminder (start! common-patient/send-reminders
                                  [patient "Please go get your labwork done"
                                   :until #(not= (-> labwork-run :status :sample) "waiting")
                                   :cancelable true]
-                                 :status {:title "Labwork"})]
+                                 :status {:title "Anticoagulation labwork"})]
     (set-status!
+      [:overview :phase] "Labwork"
       [:runs :lab :initial-tests] (:id labwork-run)
       [:runs :patient :labwork-reminder] (:id patient-reminder))
     (block! labwork-run)))
@@ -97,39 +99,60 @@
                                     :dosage :as-directed]
                                    :status {:title      "Anticoagulation prescription"
                                             :patient-id (:id patient)})]
-    (set-status! [:runs :pharmacy :warfarin-prescription]
-                 (:id prescription-phase))
+    (set-status!
+      [:overview :phase] "Prescription"
+      [:runs :pharmacy :warfarin-prescription] (:id prescription-phase))
     (block! prescription-phase)))
 
 (deflow obtain-maintenance-dosage [patient target-inr]
-  (let [run (start! patient/initiation-phase
-                    [patient target-inr]
-                    :status {:title       "Anticoagulation initiation phase"
-                             :patient-id (:id patient)})]
-    (set-status! [:runs :patient :initiation-phase] (:id run))
-    (block! run)))
+  (let [dosage-pool (->pool)
+        run         (start! patient/initiation-phase
+                            [patient target-inr dosage-pool]
+                            :status {:title      "Anticoagulation initiation phase"
+                                     :patient-id (:id patient)})]
+    (set-status!
+      [:overview :phase] "Initiation"
+      [:runs :patient :initiation-phase] (:id run))
+
+    ;; retrieve dosage until the dosage-pool returns nil
+    (loop [dosage (take-out! dosage-pool)]
+      (when dosage
+        (set-status! [:overview :dosage] dosage)
+        (recur (take-out! dosage-pool))))))
 
 (defn start-maintenance-phase [patient maintenance-dosage]
   (let [run (start! patient/maintenance-phase
                     [patient maintenance-dosage]
-                    :status {:title       "Anticoagulation maintenance phase"
+                    :status {:title      "Anticoagulation maintenance phase"
                              :patient-id (:id patient)})]
     (set-status! [:runs :patient :maintenance-phase] (:id run))))
 
+(defn check-for-existing-anticoagulation-run [patient-id]
+  (let [[run] (find-runs [[:state :eq :running]
+                          [[:status :patient-id] :eq patient-id]
+                          [:id :not-eq (current-run :id)]
+                          [[:status :roles] :? "doctor"]] :limit 2)]
+    (if run
+      (throw (ex-info "Anticoagulation therapy already in progress for patient"
+                      {:type :input-error
+                       :patient-id patient-id
+                       :run-id (:id run)})))))
+
 (deflow anticoagulation [patient-id]
   (require-roles :doctor)
-  (set-status! :patient-id patient-id :text "Anticoagulation therapy")
+  (set-status! :patient-id patient-id, :title "Anticoagulation therapy")
+  (check-for-existing-anticoagulation-run patient-id)
   (let [patient (get-patient patient-id)
-        _ (if (not (p/patient? patient)) (throw (ex-info "Patient not found" {:type :input-error :id patient-id})))
+        _       (if (not (p/patient? patient)) (throw (ex-info "Patient not found" {:type :input-error :id patient-id})))
         labwork (obtain-labwork patient [{:type :iron} {:type :cbc} {:type :kidney}])]
     (if-let [target-inr (determine-target-inr patient labwork)]
       (let [
             ;; get the initiation phase prescription - 1mg size
-            _ (obtain-warfarin-prescription patient 1)
+            _                  (obtain-warfarin-prescription patient 1)
             ;; start the initiation phase
             maintenance-dosage (obtain-maintenance-dosage patient target-inr)
             ;; get the maintenance prescription
-            _ (obtain-warfarin-prescription patient maintenance-dosage)]
+            _                  (obtain-warfarin-prescription patient maintenance-dosage)]
         (start-maintenance-phase patient maintenance-dosage)))))
 
 ;; onboarding
